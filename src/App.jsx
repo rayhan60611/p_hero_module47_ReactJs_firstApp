149a3c77294423808f28a1063e846e23c431495a
rayhan60611 <pecon.alok@gmail.com>
2023-07-02 17:38:07 +0600

    passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+const arrName = ["Rayhan", "Asif", "Tuhin", "Touhid", "Risan"];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Print name="Parvez"></Print>
+      <Print name="Rayhan"></Print>
+    </div>
+  );
 }
 
-export default App
+function Print(props) {
+  return (
+    <div className="myclass">
+      <h1>{props.name}</h1>
+    </div>
+  );
+}
+
+export default App;
